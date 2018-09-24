b9c96beb2834ce5474c218b04bc2b36a3c0131b1
Marco Montagna <marcojoemontagna@gmail.com>
2018-09-24 14:49:46 -0700

    Remove relative import.

--- generic_encryptors/encryptors/aes_encryptor.py
+++ generic_encryptors/encryptors/aes_encryptor.py
@@ -5,7 +5,7 @@ import hashlib
 import struct
 
 from generic_encoders import Encoder
-from .exceptions import HmacMissMatch
+from generic_encryptors.encryptors.exceptions import HmacMissMatch
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import padding
