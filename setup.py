93b4688614c620597a4b46e9729309f9e4c25561
Marco Montagna <marcojoemontagna@gmail.com>
2019-07-12 17:08:40 -0700

    Relax cryptography constraint

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
   python_requires=">=2.7",
   license=open(os.path.join(dir_path, 'LICENSE')).read(),
   install_requires=[
-    "cryptography~=2.3.0",
+    "cryptography>=2.3.0",
     "generic-encoders<1.0"
   ],
   entry_points = {
